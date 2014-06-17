7fceb85c82da8aa73a8d742f41db8d7575464ccb
Andros Rosa <andros2011@gmail.com>
2014-06-16 22:14:45 -0400

    Don't include common.mk twice
    common_phone.mk already calls it
    
    Change-Id: I7e4351ea592e1043da8dc1b45062496781a9591b

--- carbon.mk
+++ carbon.mk
@@ -5,12 +5,11 @@ PRODUCT_RELEASE_NAME := i9500
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common Carbon stuff.
-$(call inherit-product, vendor/carbon/config/common.mk)
-$(call inherit-product, vendor/carbon/config/common_phone.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/samsung/i9500/full_i9500.mk)
+
+# Inherit some common Carbon stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9500
